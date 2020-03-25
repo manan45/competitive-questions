308564101f977862265e3703c6bd3fa8a026ac2f
mananclassplus <manan@classplus.co>
2020-03-25 21:08:34 +0530

    prblem demo

---
+++ array/problem1.py
@@ -0,0 +1,16 @@
+
+# Problem statement - Addition of Two numbers
+# problem expected Input - varibales a, b
+# problem expected Output - a+b
+
+# additional infomation
+
+
+# start code here
+
+def add(integer1, integer2):
+    return integer1+integer2
+
+
+result = add(1, 2)
+print(result)
